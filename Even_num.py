55f177546f97c6d3d628fc97cfb3dd69129d1456
HomagoDon <miguelsalazardebarros@gmail.com>
2022-12-01 11:03:11 -0300

    First Commit

---
+++ Even_num.py
@@ -0,0 +1,11 @@
+# this checks if both numbers are even
+# if both are then it returns the lower number, if not, it returns the bigger number
+
+def lesser_of_two_evens(a,b):
+    if a % 2 == 0 and b % 2 == 0:
+
+        return min(a,b)
+
+    else:
+
+        return max(a,b)
